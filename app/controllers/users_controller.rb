a03dca2f4546e17f61e1649fa2790e748ff51b26
Dani Wilson <danirae.powell@gmail.com>
2023-11-30 15:13:21 -0700

    add new, show, create actions and private method

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  def show
+    @user = current_user
+  end
+  
+  def new
+    @user = User.new
+  end
+
+  def create 
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id 
+      flash[:message] = "Account successfully created!"
+      redirect_to user_path(@user.id)
+    else
+      flash[:error] = "Fields cannot be blank."
+      redirect_to new_user_path
+    end
+  end
+end
+
+
+private
+
+def user_params
+  params.require(:user).permit(:username, :email, :password, :password_confirmation)
+end
