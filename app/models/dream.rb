e4f001ec4390199069787d50ea843b2d5a70ac02
Dani Wilson <danirae.powell@gmail.com>
2023-11-30 15:11:53 -0700

    edit order of validations

--- app/models/dream.rb
+++ app/models/dream.rb
@@ -1,11 +1,11 @@
 class Dream < ApplicationRecord
   validates_presence_of :dream_title 
+  validates_presence_of :user_id
   validates_presence_of :dream_content
   validates_presence_of :dream_date
   validates_presence_of :privacy_setting
   validates_presence_of :favorite
   validates_presence_of :additional_insights
-  validates_presence_of :user_id
 
   belongs_to :user
 end
