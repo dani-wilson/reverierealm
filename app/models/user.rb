4bb97f0a2697264b7fa3cc89ee6e34cce1ba24ee
Dani Wilson <danirae.powell@gmail.com>
2023-11-30 15:12:11 -0700

    add better validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   has_secure_password 
   
-  validates_presence_of :username
-  validates_presence_of :email
+  validates_presence_of :username, allow_nil: false
+  validates_presence_of :email, uniqueness: true
+  validates :password_digest, presence: true, confirmation: true
+  has_secure_password
 
   has_many :dreams
 end
