5d07b114bc58d2bc0cabadab0efc6af3dcb270a9
Dani Wilson <danirae.powell@gmail.com>
2023-12-13 16:54:11 -0700

    add validation for unique username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password 
   
-  validates_presence_of :username, allow_nil: false
+  validates_presence_of :username, allow_nil: false, uniqueness: true
   validates :email,
   format: { with: /\A(.+)@(.+)\z/ },
   uniqueness: true,
