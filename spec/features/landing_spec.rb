17b2e351e1f7523d38ed98e2bcd5ccddd10bb993
Dani Wilson <danirae.powell@gmail.com>
2023-12-13 16:50:41 -0700

    Add sad path test for username uniqueness

--- spec/features/landing_spec.rb
+++ spec/features/landing_spec.rb
@@ -44,6 +44,32 @@ RSpec.describe "The landing page" do
       expect(current_path).to eq(user_path(User.last.id))
     end
 
+    xit "SAD PATH: My username must be unique" do
+      visit "/"
+
+      click_link "Create an Account"
+
+      fill_in('Username', with: "DreamyDani")
+      fill_in('Email', with: "dreams@test.com")
+      fill_in('Password', with: "password123")
+      fill_in('Confirm Password', with: "password123")
+
+      click_button("Create Account")
+
+      visit "/"
+
+      click_link "Create an Account"
+
+      fill_in('Username', with: "DreamyDani")
+      fill_in('Email', with: "danio@test.com")
+      fill_in('Password', with: "password123")
+      fill_in('Confirm Password', with: "password123")
+
+      click_button("Create Account")
+
+      expect(page).to have_content("User already exists.")
+    end
+
     it "SAD PATH: when I fill out the form incorrectly, I see a message notifying me that fields cannot be blank" do
       visit "/"
 
@@ -87,6 +113,49 @@ RSpec.describe "The landing page" do
 
       expect(page).to have_content("User must enter a valid email address.")
       expect(current_path).to eq(new_user_path)
+    end
+  end
+
+  describe "logging in" do
+    before(:each) do
+      visit "/"
+
+      click_link "Create an Account"
+
+      expect(current_path).to eq(new_user_path)
+
+      fill_in('Username', with: "DreamyDani")
+      fill_in('Email', with: "dreams@test.com")
+      fill_in('Password', with: "password123")
+      fill_in('Confirm Password', with: "password123")
+
+      click_button("Create Account")
+
+      @user = User.first
+    end
+
+    it "HAPPY PATH: has the option to login with an already-made account" do
+      visit "/"
+
+      expect(page).to have_link("Log In")
+
+      click_link('Log In')
+
+      expect(current_path).to eq(login_path)
+    end
+
+    it "HAPPY PATH: can log in" do
+      visit "/"
+
+      click_link("Log In")
+
+      fill_in('Username', with: 'DreamyDani')
+      fill_in('Password', with: 'password123')
+      click_button('Log In')
+
+      expect(page).to have_content("Welcome, #{@user.username}")
+
+      expect(current_path).to eq(user_path(@user))
     end
   end
 end
