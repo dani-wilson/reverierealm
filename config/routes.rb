5ae00bc4ceba3c9db69bd864e41f6a122f16f56d
Dani Wilson <danirae.powell@gmail.com>
2023-12-13 16:49:54 -0700

    add login routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root "landing#index"
 
   resources :users, only: [:new, :create, :show]
+
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
 end
