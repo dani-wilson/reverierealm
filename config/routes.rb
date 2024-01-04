6a4a1ede597eab1f5b4f46ecc9e7a1699ef91aa8
Dani Wilson <danirae.powell@gmail.com>
2024-01-04 12:16:44 -0700

    add routes for dream new and create

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create, :show]
 
+  resources :dreams, only: [:new, :create]
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
