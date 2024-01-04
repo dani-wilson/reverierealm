65fcbbffabe45f30de7d522318c02f8b9e4522e4
Dani Wilson <danirae.powell@gmail.com>
2024-01-04 16:44:31 -0700

    add create action and private params

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -3,5 +3,20 @@ class DreamsController < ApplicationController
   end
 
   def create
+    @user = current_user
+    # require 'pry'; binding.pry
+    @dream = @user.dreams.new(dream_params)
+    if @dream.save
+      flash[:alert] = "Entry saved."
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  private 
+
+  def dream_params
+    params.permit(:dream_title, :dream_date, :dream_content)
   end
 end
