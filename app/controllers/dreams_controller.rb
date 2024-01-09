70ac878bf8bb0c0c59ef34c4589a4275783ec983
Dani Wilson <danirae.powell@gmail.com>
2024-01-09 10:30:06 -0700

    Edit create action to remove date field

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -1,11 +1,12 @@
 class DreamsController < ApplicationController
   def new
+    @dream = Dream.new
   end
 
   def create
     @user = current_user
-    # require 'pry'; binding.pry
     @dream = @user.dreams.new(dream_params)
+  
     if @dream.save
       flash[:alert] = "Entry saved."
       redirect_to user_path(@user)
@@ -13,10 +14,11 @@ class DreamsController < ApplicationController
       render :new
     end
   end
+  
 
-  private 
+  private
 
   def dream_params
-    params.permit(:dream_title, :dream_date, :dream_content)
-  end
+    params.require(:dream).permit(:dream_title, :dream_date, :dream_content, :additional_insights, :privacy_setting, :favorite)
+  end  
 end
