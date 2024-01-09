45a0eb1b605ac8262e4147bb8fd0094c5066f30b
Dani Wilson <danirae.powell@gmail.com>
2024-01-09 10:29:20 -0700

    test: add tests for viewing new saved entry and allow blank

--- spec/features/show_spec.rb
+++ spec/features/show_spec.rb
@@ -45,9 +45,48 @@ RSpec.describe "The Show Page" do
     fill_in("Date of Dream", with: "01/03/2024")
     fill_in("Dream Content", with: "I'm at some sort of large convention. Someone I know from highschool is here and has made four different kinds of soups. He wants me to try them all, and I do, and they're extremely delicious. He watches me eat them with fondness.")
     fill_in("Additional Insights", with: "Soup symbolizes emotional nourishment and comfort. Tomatoes represent domestic life (which was one of the soup flavors.)")
+    check("Private?")
+    check("Favorite")
 
     click_button("Create Entry")
-      save_and_open_page
+
+    expect(page).to have_content("Entry saved.")
+    
     expect(current_path).to eq(user_path(@user))
+  end
+
+  it "once I've submitted a dream, I see it on my dashboard" do
+    click_button("New Entry")
+
+    fill_in("Title", with: "Winter Garden")
+    fill_in("Date of Dream", with: "12/31/2022")
+    fill_in("Dream Content", with: "It's icy and there's snow everywhere, but I find a patch of lettuce and carrots growing. I feel so happy")
+    fill_in("Additional Insights", with: "Good stuff")
+    check("Private?")
+    check("Favorite")
+
+    click_button("Create Entry")
+    
+    expect(page).to have_content("Entry saved.")
+
+    expect(page).to have_content("Winter Garden")
+    expect(page).to have_content("It's icy and there's snow everywhere,")
+    expect(page).to have_content("12/31/2022")
+  end
+
+  it "I do not need to have all fields filled out to submit an entry" do
+    click_button("New Entry")
+
+    fill_in("Title", with: "A cute puppy")
+    fill_in("Date of Dream", with: "07/21/2023")
+    fill_in("Dream Content", with: "There's a cute white puppy and she bites really hard.")
+
+    click_button("Create Entry")
+    
+    expect(page).to have_content("Entry saved.")
+
+    expect(page).to have_content("A cute puppy")
+    expect(page).to have_content("There's a cute white puppy and")
+    expect(page).to have_content("07/21/2023")
   end
 end
