70e16bb4959c203b1c92fe584f4906b2ed044f57
Dani Wilson <danirae.powell@gmail.com>
2024-01-09 12:37:07 -0700

    edit validations

--- app/models/dream.rb
+++ app/models/dream.rb
@@ -3,9 +3,9 @@ class Dream < ApplicationRecord
   validates_presence_of :user_id
   validates_presence_of :dream_content
   validates_presence_of :dream_date
-  validates_presence_of :privacy_setting
-  validates_presence_of :favorite
-  validates_presence_of :additional_insights
+  validates_presence_of :privacy_setting, allow_blank: true
+  validates_presence_of :favorite, allow_blank: true
+  validates_presence_of :additional_insights, allow_blank: true
 
   belongs_to :user
 end
