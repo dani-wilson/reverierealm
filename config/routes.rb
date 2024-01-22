06463e5910f21b27acf0d2e2828e4b52c355153a
Dani Wilson <danirae.powell@gmail.com>
2024-01-22 11:39:25 -0700

    add show route for dreams controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create, :show]
 
-  resources :dreams, only: [:new, :create]
+  resources :dreams, only: [:new, :create, :show]
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
